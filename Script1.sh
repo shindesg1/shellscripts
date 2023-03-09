e426f689f6555c82c97b45b0ed722e1c36a36e4f
sachin <sachin61091@gmail.com>
2023-03-09 09:50:51 -0800

    this is another commit

--- Script1.sh
+++ Script1.sh
@@ -9,4 +9,5 @@ echo "We are done with git"
 echo "This is sachin shinde"
 echo "I complted BE in 2013"
 echo "After that i have complted Mtech in 2016"
-
+echo "we are changing sctipt 1"
+echo "we have applied this changes in main branch"
